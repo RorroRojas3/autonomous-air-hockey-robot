c906c1e6a22e93f4e21441225d575050952bbf3f
RorroRojas3 <rrojas@clemson.edu>
2018-09-26 16:35:35 -0400

    homography not works

--- air_hockey/PSEyeDemo/air_hockey.cpp
+++ air_hockey/PSEyeDemo/air_hockey.cpp
@@ -16,8 +16,12 @@ const String DILUTION_WINDOW_NAME = "Dilusion Values";
 const String DILUTE_WINDOW_NAME = "Dilute Values";
 const String CAMERA_WINDOW_NAME = "PS3 Camera";
 int const MAX_KERNEL_SIZE = 21;
+double const scale = 5.0;
 int erosion_val = 0;
 int dilation_val = 0;
+
+// Typedefs
+typedef vector<Point2f> Point2fVector;
 
 // Global Variables
 int low_H = 0, low_S = 0, low_V = 0;
@@ -26,18 +30,22 @@ Mat current_frame;
 Mat hsv_frame;
 Mat frame_threshold;
 Mat blur_frame;
-
+Mat unwarp_frame;
+Mat_<double> H;
+Point2fVector points;
+Point2fVector points2;
+Mat Frame;
 
 #define FRAME_RATE 60
 #define RESOLUTION CLEYE_VGA
-// QVGA or VGA
 
-typedef struct{
+typedef struct
+{
 	CLEyeCameraInstance CameraInstance;
 	Mat *Frame;
 	unsigned char *FramePtr;
 	int Threshold;
-}CAMERA_AND_FRAME;
+} CAMERA_AND_FRAME;
 
 static DWORD WINAPI CaptureThread(LPVOID ThreadPointer);
 
@@ -111,7 +119,7 @@ void dilation_trackbars()
 
 void MousCallback(int mEvent, int x, int y, int flags, void* param)
 {
-	vector<Point2f> * pPointVec = (vector<Point2f>*)param;
+	Point2fVector* pPointVec = (Point2fVector*)param;
 	if (mEvent == CV_EVENT_LBUTTONDOWN)
 	{
 		pPointVec->push_back(Point2f(float(x), float(y)));
@@ -124,7 +132,6 @@ int _tmain(int argc, _TCHAR* argv[])
 	int Width,Height;
 	int KeyPress;
 	CLEyeCameraInstance EyeCamera=NULL;
-	Mat Frame;
 	CAMERA_AND_FRAME ThreadPointer;
 	HANDLE _hThread;
 	CLEyeCameraParameter CamCurrentParam=(CLEyeCameraParameter)0;
@@ -142,19 +149,20 @@ int _tmain(int argc, _TCHAR* argv[])
 	CLEyeCameraGetFrameDimensions(EyeCamera, Width, Height);
 	// Create a window in which the captured images will be presented
 	namedWindow( "Camera", CV_WINDOW_AUTOSIZE );
-	namedWindow("Thresh", CV_WINDOW_AUTOSIZE);
+	namedWindow("Threshold", CV_WINDOW_AUTOSIZE);
 	//Make a image to hold the frames captured from the camera
-	Frame=Mat(Height,Width,CV_8UC4);//8 bit unsiged 4 channel image for Blue Green Red Alpa (8 bit elements per channel)
+	Frame = Mat(Height, Width, CV_8UC4);//8 bit unsiged 4 channel image for Blue Green Red Alpa (8 bit elements per channel)
 	//Start the eye camera
 	CLEyeCameraStart(EyeCamera);
 
 	/////////////////////////////////////MAIN CODE//////////////////////////////////////
-
+	
 
 	//Need to copy vars into one var to launch the second thread
-	ThreadPointer.CameraInstance=EyeCamera;
+	ThreadPointer.CameraInstance = EyeCamera;
 	ThreadPointer.Frame = &Frame;
-	ThreadPointer.Threshold=0;
+	ThreadPointer.Threshold = 0;
+
 	//Launch thread and confirm its running
 	_hThread = CreateThread(NULL, 0, &CaptureThread, &ThreadPointer, 0, 0);
 	if(_hThread == NULL)
@@ -163,34 +171,6 @@ int _tmain(int argc, _TCHAR* argv[])
 		getchar();
 		return false;
 	}
-
-	/*
-	imshow("Camera", Frame);
-	MessageBoxA(NULL, "Pick four corners on the video", "Click", MB_OK);
-
-	vector<Point2f> points;
-
-	cvSetMouseCallback("Camera", MousCallback, &points);
-
-	while (1)
-	{
-		// wait for mouse clicks
-		waitKey(10);
-		if (points.size() == 4)
-		{
-			break;
-		}
-	}
-
-	// HERE I ASSUME EACH PIXEL WILL BE 5 mm
-	double scale = 5.0;
-	vector<Point2f> points2;
-	points2.push_back(Point2f(0.0, 2000.0 / scale));
-	points2.push_back(Point2f(1200.0 / scale, 2000.0 / scale));
-	points2.push_back(Point2f(1200.0 / scale, 0.0));
-	points2.push_back(Point2f(0.0, 0.0));
-
-	Mat_<double> H = findHomography(Mat(points), Mat(points2));
 
 	/* CREATE TRACK BAR FOR HSV THRESHOLD VALUES*/
 	hsv_trackbars();
@@ -203,39 +183,6 @@ int _tmain(int argc, _TCHAR* argv[])
 	
 	while( 1 ) 
 	{
-		/*
-		// Creates a copy of the current video input from PS3 camera
-		current_frame = Frame.clone();
-		
-		// Changes video input from RGB to HSV
-		cvtColor(Frame, current_frame, CV_RGB2HSV);
-
-		// Change the Threshold for HSV frame with TrackBards
-		inRange(current_frame, Scalar(low_H, low_S, low_V), Scalar(high_H, high_S, high_V), frame_threshold);
-
-		//warpPerspective(frame_threshold, frame_threshold, H, Size(1200.0 / scale, 2000.0 / scale));
-
-		// Starting point to erode video
-		erode_video(0, 0);
-
-		// Starting point to dilute video
-		dilute_video(0, 0);
-
-		// Draw circle
-
-		GaussianBlur(frame_threshold, frame_threshold, Size(9, 9), 2, 2);
-		vector<Vec3f> circles;
-
-		HoughCircles(frame_threshold, circles, CV_HOUGH_GRADIENT, 1, frame_threshold.rows / 8, 200, 100, 0, 0);
-
-		for (int c1 = 0; c1 < circles.size(); c1++)
-		{
-			Point center(cvRound(circles[c1][0]), cvRound(circles[c1][1]));
-			int radius = cvRound(circles[c1][2]);
-			circle(Frame, center, 3, Scalar(0, 0, 255), -1, 8, 0);
-			circle(Frame, center, radius, Scalar(0, 0, 255), 3, 8, 0);
-		}
-		*/
 		
 		//This will capture keypresses and do whatever you want if you assign the appropriate actions to the right key code
 		KeyPress = waitKey(1);
@@ -248,12 +195,8 @@ int _tmain(int argc, _TCHAR* argv[])
 				break;
 		}
 
-		//Display the captured frame
-		//imshow("PS3 Camera- RGB", Frame);
-		//imshow("PS3 Camera - HSV", current_frame);
-		//imshow(CAMERA_WINDOW_NAME, frame_threshold);
 		imshow("Camera", Frame);
-		imshow("Tresh", frame_threshold);
+		imshow("Threshold", frame_threshold);
 	}
 	
 	CLEyeCameraStop(EyeCamera);
@@ -265,56 +208,90 @@ int _tmain(int argc, _TCHAR* argv[])
 
 ///////////////////////SUB THREAD///////////////////////////
 //for high frame rates you will process images here the main function will allow interactions and display only
-static DWORD WINAPI CaptureThread(LPVOID ThreadPointer){
+static DWORD WINAPI CaptureThread(LPVOID ThreadPointer)
+{
 	CAMERA_AND_FRAME *Instance=(CAMERA_AND_FRAME*)ThreadPointer; //type cast the void pointer back to the proper type so we can access its elements
 
 	int FramerCounter=0;
 	Mat CamImg=Mat(*(Instance->Frame)).clone();
 
-	clock_t StartTime,EndTime;
+	clock_t StartTime, EndTime; 
+
+	// Pick four points on Homography 
+	/*namedWindow("Homography Camera", CV_WINDOW_AUTOSIZE);
+	cvShowImage("Homography Camera", &(IplImage)CamImg);
+	MessageBoxA(NULL, "Please click four corners of the table", "Click", MB_OK);
+	cvSetMouseCallback("Homography Camera", MousCallback, &points);
+
+	while (1)
+	{
+		// wait for mouse clicks
+		waitKey(10);
+		if (points.size() == 4)
+		{
+			break;
+		}
+	}
+
+	// Calculate Homography matrix
+	points2.push_back(Point2f(0.0, 2000.0 / scale));
+	points2.push_back(Point2f(1200.0 / scale, 2000.0 / scale));
+	points2.push_back(Point2f(1200.0 / scale, 0.0));
+	points2.push_back(Point2f(0.0, 0.0));
+	H = findHomography(Mat(points), Mat(points2));*/
+
 	while(1)
 	{
-		//Get Frame From Camera
-		CLEyeCameraGetFrame(Instance->CameraInstance,CamImg.data);
+		//if (points.size() == 4)
+		//{
+			//Get Frame From Camera
+			CLEyeCameraGetFrame(Instance->CameraInstance, CamImg.data);
 
-		// DO YOUR IMAGE PROCESSING HERE
-		cvtColor(CamImg, hsv_frame, CV_RGB2HSV);
+			// Homography frame
+			//warpPerspective(CamImg, unwarp_frame, H, Size(1200.0 / scale, 2000.0 / scale));
 
-		inRange(hsv_frame, Scalar(low_H, low_S, low_V), Scalar(high_H, high_S, high_V), frame_threshold);
+			// DO YOUR IMAGE PROCESSING HERE
+			cvtColor(CamImg, hsv_frame, CV_RGB2HSV);
 
-		// Starting point to erode video
-		//erode_video(0, 0);
+			inRange(hsv_frame, Scalar(low_H, low_S, low_V), Scalar(high_H, high_S, high_V), frame_threshold);
 
-		// Starting point to dilute video
-		//dilute_video(0, 0);
+			// Starting point to erode video
+			erode_video(0, 0);
 
-		GaussianBlur(frame_threshold, blur_frame, Size(9, 9), 2, 2);
-		vector<Vec3f> circles;
+			// Starting point to dilute video
+			dilute_video(0, 0);
 
-		HoughCircles(blur_frame, circles, CV_HOUGH_GRADIENT, 1, frame_threshold.rows / 8, 200, 15, 125, 200);
-		//cvShowImage("Blur", blur_frame);
+			GaussianBlur(frame_threshold, blur_frame, Size(9, 9), 2, 2);
+			vector<Vec3f> circles;
 
-		//cout << circles.size() << endl;
-		if (circles.size() == 1)
-		{
-			cout << "Circle size: " << circles.size() << endl;
-			Point center(cvRound(circles[0][0]), cvRound(circles[0][1]));
-			int radius = cvRound(circles[0][2]);
-			circle(CamImg, center, 3, Scalar(0, 0, 255), -1, 8, 0);
-			circle(CamImg, center, radius, Scalar(0, 0, 255), 3, 8, 0);
-		}
-		
-		//copy it to main thread image.
-		*(Instance->Frame) = CamImg;
+			HoughCircles(blur_frame, circles, CV_HOUGH_GRADIENT, 1, frame_threshold.rows / 8, 200, 15, 125, 130);
+
+			if (circles.size() == 1)
+			{
+				cout << "Circle size: " << circles.size() << endl;
+				Point center(cvRound(circles[0][0]), cvRound(circles[0][1]));
+				int radius = cvRound(circles[0][2]);
+				circle(CamImg, center, 3, Scalar(0, 0, 255), -1, 8, 0);
+				circle(CamImg, center, radius, Scalar(0, 0, 255), 3, 8, 0);
+			}
+
+			//copy it to main thread image.
+			*(Instance->Frame) = CamImg;
+		//}
 
 		// Track FPS
-		if(FramerCounter==0) StartTime=clock();
+		if (FramerCounter == 0)
+		{
+			StartTime = clock();
+		}
 		FramerCounter++;
 		EndTime=clock();
-		if((EndTime-StartTime)/CLOCKS_PER_SEC>=1){
+		if((EndTime-StartTime)/CLOCKS_PER_SEC>=1)
+		{
 			//cout << "FPS:" << FramerCounter << endl;
 			FramerCounter=0;
 		}
+
 	}
 	return 0;
 }
