718f276ba1b95b291961a07c82345bae8b7405e3
RorroRojas3 <rrojas@clemson.edu>
2018-09-27 07:58:45 -0400

    clean up code

--- air_hockey/PSEyeDemo/air_hockey.cpp
+++ air_hockey/PSEyeDemo/air_hockey.cpp
@@ -1,6 +1,4 @@
-// OpenCVCam.cpp : Defines the entry point for the console application.
-//
-
+// Library Declaration Section
 #include "stdafx.h"
 #include "PSEyeDemo.h"
 
@@ -17,8 +15,7 @@ const String DILUTE_WINDOW_NAME = "Dilute Values";
 const String CAMERA_WINDOW_NAME = "PS3 Camera";
 int const MAX_KERNEL_SIZE = 21;
 double const scale = 5.0;
-int erosion_val = 0;
-int dilation_val = 0;
+
 
 // Typedefs
 typedef vector<Point2f> Point2fVector;
@@ -26,6 +23,8 @@ typedef vector<Point2f> Point2fVector;
 // Global Variables
 int low_H = 0, low_S = 0, low_V = 0;
 int high_H = MAX_VALUE_H, high_S = MAX_VALUE, high_V = MAX_VALUE;
+int erosion_val = 0;
+int dilation_val = 0;
 Mat current_frame;
 Mat hsv_frame;
 Mat frame_threshold;
@@ -49,6 +48,7 @@ typedef struct
 
 static DWORD WINAPI CaptureThread(LPVOID ThreadPointer);
 
+/*** HSV TRACKBAR CALLBACK FUNCTIONS ***/
 static void low_H_callBack(int, void *)
 {
 	low_H = min(high_H - 1, low_H);
@@ -79,19 +79,21 @@ static void high_V_callBack(int, void *)
 	high_V = max(high_V, low_V + 1);
 }
 
+/*** ERODE CALLBACK FUNCTION ***/
 static void erode_video(int , void *)
 {
 	Mat element = getStructuringElement(MORPH_ELLIPSE, Size(2 * erosion_val + 1, 2 * erosion_val + 1), Point(erosion_val, erosion_val));
 	erode(frame_threshold, frame_threshold, element);
 }
 
+/*** DILUTE CALLBACK FUNCTION ***/
 static void dilute_video(int, void *)
 {
 	Mat element = getStructuringElement(MORPH_ELLIPSE, Size(2 * dilation_val + 1, 2 * dilation_val + 1), Point(dilation_val, dilation_val));
 	dilate(frame_threshold, frame_threshold, element);
 }
 
-// Creates HSV video Trackbars
+/*** CREATES TRACKBARS FOR HSV VALUES ***/
 void hsv_trackbars()
 {
 	namedWindow(HSV_WINDOW_NAME);
@@ -103,20 +105,21 @@ void hsv_trackbars()
 	createTrackbar("High V", HSV_WINDOW_NAME, &high_V, MAX_VALUE, high_V_callBack);
 }
 
-// Creates Erosion video Trackbars
+/*** CREATES TRACKBAR FOR EROSION VALUE ***/
 void erosion_trackbars()
 {
 	namedWindow(EROSION_WINDOW_NAME);
 	createTrackbar("Kernel Size [2n + 1]", EROSION_WINDOW_NAME, &erosion_val, MAX_KERNEL_SIZE, erode_video);
 }
 
-// Creates Dilusion video Trackbars
+/*** CREATES DILATION FOR EROSION VALUE ***/
 void dilation_trackbars()
 {
 	namedWindow(DILUTION_WINDOW_NAME);
 	createTrackbar("Kernel Size [2n + 1]", DILUTION_WINDOW_NAME, &erosion_val, MAX_KERNEL_SIZE, dilute_video);
 }
 
+/*** ALLOWS USER TO PICK FOUR POINTS FROM PS3 CAMERA FRAME ***/
 void MousCallback(int mEvent, int x, int y, int flags, void* param)
 {
 	Point2fVector* pPointVec = (Point2fVector*)param;
@@ -126,43 +129,21 @@ void MousCallback(int mEvent, int x, int y, int flags, void* param)
 	}
 }
 
-int _tmain(int argc, _TCHAR* argv[])
+/*** CALCULATES HOMOGRAPHY  ***/
+void calc_homography(CLEyeCameraInstance *ps3_camera)
 {
-	// Variable Declaration Section
-	int Width,Height;
-	int KeyPress;
-	CLEyeCameraInstance EyeCamera=NULL;
-	CAMERA_AND_FRAME ThreadPointer;
-	HANDLE _hThread;
-	CLEyeCameraParameter CamCurrentParam=(CLEyeCameraParameter)0;
-	bool CamParam=0;
-	
+	// Get current frame from PS3 camera
+	CLEyeCameraGetFrame(ps3_camera, Frame.data);
 
-	//////////////////////
-
-	//////////////////// EYE CAMERA SETUP///////////////////////////////////
-	// all of this code and more is included in my header file CameraControl.h hence why its commented out
-	// I left it here simply for your reference
-	EyeCamera=StartCam(FRAME_RATE,RESOLUTION);//this does all the commented out code
-
-	// Get camera frame dimensions;
-	CLEyeCameraGetFrameDimensions(EyeCamera, Width, Height);
-	// Create a window in which the captured images will be presented
-	namedWindow( "Camera", CV_WINDOW_AUTOSIZE );
-	namedWindow("Threshold", CV_WINDOW_AUTOSIZE);
-	//Make a image to hold the frames captured from the camera
-	Frame = Mat(Height, Width, CV_8UC4);//8 bit unsiged 4 channel image for Blue Green Red Alpa (8 bit elements per channel)
-	//Start the eye camera
-	CLEyeCameraStart(EyeCamera);
-
-	CLEyeCameraGetFrame(EyeCamera, Frame.data);
-	/////////////////////////////////////MAIN CODE//////////////////////////////////////
-	// Pick four points on Homography 
+	// Create a window and display the PS3 current frame
 	namedWindow("Homography Camera", CV_WINDOW_AUTOSIZE);
 	cvShowImage("Homography Camera", &(IplImage)Frame);
-	//MessageBoxA(NULL, "Please click four corners of the table", "Click", MB_OK);
+
+	// Asks user to pick four points from Homography window (top-left, top-right, bottom-right, bottom-left)
+	MessageBoxA(NULL, "Please click four corners of the table", "Click", MB_OK);
 	cvSetMouseCallback("Homography Camera", MousCallback, (void *)&points);
 
+	// Waits until the user has clicked the four points
 	while (1)
 	{
 		// wait for mouse clicks
@@ -178,14 +159,54 @@ int _tmain(int argc, _TCHAR* argv[])
 	points2.push_back(Point2f(640, 0));
 	points2.push_back(Point2f(0, 480));
 	points2.push_back(Point2f(640, 480));
-	H = findHomography(Mat(points), Mat(points2));
 
-	//Need to copy vars into one var to launch the second thread
-	ThreadPointer.CameraInstance = EyeCamera;
+	// Calculate homography
+	H = findHomography(Mat(points), Mat(points2));
+}
+
+/*** TERMINATES PS3 CAMERA ***/
+void terminate_ps3camera(CLEyeCameraInstance *ps3_camera)
+{
+	CLEyeCameraStop(ps3_camera);
+	CLEyeDestroyCamera(ps3_camera);
+	ps3_camera = NULL;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	// Variable Declaration Section
+	int width, height;
+	int press_key;
+	CLEyeCameraInstance ps3_camera = NULL;
+	CAMERA_AND_FRAME ThreadPointer;
+	HANDLE _hThread;
+	CLEyeCameraParameter CamCurrentParam=(CLEyeCameraParameter)0;
+	bool CamParam=0;
+
+	/*** PS3 CAMERA SETUP ***/
+	ps3_camera = StartCam(FRAME_RATE,RESOLUTION);//this does all the commented out code
+
+	// Get camera frame dimensions;
+	CLEyeCameraGetFrameDimensions(ps3_camera, width, height);
+	// Create a window in which the captured images will be presented
+	namedWindow( "Camera", CV_WINDOW_AUTOSIZE );
+	namedWindow("Threshold", CV_WINDOW_AUTOSIZE);
+	//Make a image to hold the frames captured from the camera
+	Frame = Mat(height, width, CV_8UC4);//8 bit unsiged 4 channel image for Blue Green Red Alpa (8 bit elements per channel)
+	//Start the eye camera
+	CLEyeCameraStart(ps3_camera);
+
+	/*** ALGORITHM STARTS HERE ***/
+
+	// Pick four points on Homography 
+	calc_homography(&ps3_camera);
+
+	// Copies variables for the new thread to be created 
+	ThreadPointer.CameraInstance = ps3_camera;
 	ThreadPointer.Frame = &Frame;
 	ThreadPointer.Threshold = 0;
 
-	//Launch thread and confirm its running
+	//Creates a new thread which continously works with the PS3 camera frames
 	_hThread = CreateThread(NULL, 0, &CaptureThread, &ThreadPointer, 0, 0);
 	if(_hThread == NULL)
 	{
@@ -205,10 +226,9 @@ int _tmain(int argc, _TCHAR* argv[])
 	
 	while( 1 ) 
 	{
-		
 		//This will capture keypresses and do whatever you want if you assign the appropriate actions to the right key code
-		KeyPress = waitKey(1);
-		switch (KeyPress)
+		press_key = waitKey(1);
+		switch (press_key)
 		{
 			case 27: //escape pressed
 				return 0;
@@ -221,9 +241,8 @@ int _tmain(int argc, _TCHAR* argv[])
 		imshow("Threshold", frame_threshold);
 	}
 	
-	CLEyeCameraStop(EyeCamera);
-	CLEyeDestroyCamera(EyeCamera);
-	EyeCamera = NULL;
+	/*** CLOSES CAMERA  ***/
+	terminate_ps3camera(&ps3_camera);
 
 	return 0;
 }
@@ -241,44 +260,41 @@ static DWORD WINAPI CaptureThread(LPVOID ThreadPointer)
 
 	while(1)
 	{
-		//if (points.size() == 4)
-		//{
-			//Get Frame From Camera
-			CLEyeCameraGetFrame(Instance->CameraInstance, CamImg.data);
+		//Get Frame From Camera
+		CLEyeCameraGetFrame(Instance->CameraInstance, CamImg.data);
 
-			// Homography frame
-			warpPerspective(CamImg, unwarp_frame, H, Size(1200.0 / scale, 2000.0 / scale));
+		// Homography frame
+		warpPerspective(CamImg, unwarp_frame, H, Size(1200.0 / scale, 2000.0 / scale));
 
-			cvShowImage("Homography Camera", &(IplImage)unwarp_frame);
+		cvShowImage("Homography Camera", &(IplImage)unwarp_frame);
 
-			// DO YOUR IMAGE PROCESSING HERE
-			cvtColor(unwarp_frame, hsv_frame, CV_RGB2HSV);
+		// DO YOUR IMAGE PROCESSING HERE
+		cvtColor(unwarp_frame, hsv_frame, CV_RGB2HSV);
 
-			inRange(hsv_frame, Scalar(low_H, low_S, low_V), Scalar(high_H, high_S, high_V), frame_threshold);
+		inRange(hsv_frame, Scalar(low_H, low_S, low_V), Scalar(high_H, high_S, high_V), frame_threshold);
 
-			// Starting point to erode video
-			erode_video(0, 0);
+		// Starting point to erode video
+		erode_video(0, 0);
 
-			// Starting point to dilute video
-			dilute_video(0, 0);
+		// Starting point to dilute video
+		dilute_video(0, 0);
 
-			GaussianBlur(frame_threshold, blur_frame, Size(9, 9), 2, 2);
-			vector<Vec3f> circles;
+		GaussianBlur(frame_threshold, blur_frame, Size(9, 9), 2, 2);
+		vector<Vec3f> circles;
 
-			HoughCircles(blur_frame, circles, CV_HOUGH_GRADIENT, 1, frame_threshold.rows / 8, 200, 15, 125, 130);
+		HoughCircles(blur_frame, circles, CV_HOUGH_GRADIENT, 1, frame_threshold.rows / 8, 200, 15, 125, 130);
 
-			if (circles.size() == 1)
-			{
-				cout << "Circle size: " << circles.size() << endl;
-				Point center(cvRound(circles[0][0]), cvRound(circles[0][1]));
-				int radius = cvRound(circles[0][2]);
-				circle(CamImg, center, 3, Scalar(0, 0, 255), -1, 8, 0);
-				circle(CamImg, center, radius, Scalar(0, 0, 255), 3, 8, 0);
-			}
+		if (circles.size() == 1)
+		{
+			cout << "Circle size: " << circles.size() << endl;
+			Point center(cvRound(circles[0][0]), cvRound(circles[0][1]));
+			int radius = cvRound(circles[0][2]);
+			circle(CamImg, center, 3, Scalar(0, 0, 255), -1, 8, 0);
+			circle(CamImg, center, radius, Scalar(0, 0, 255), 3, 8, 0);
+		}
 
-			//copy it to main thread image.
-			*(Instance->Frame) = CamImg;
-		//}
+		//copy it to main thread image.
+		*(Instance->Frame) = CamImg;
 
 		// Track FPS
 		if (FramerCounter == 0)
