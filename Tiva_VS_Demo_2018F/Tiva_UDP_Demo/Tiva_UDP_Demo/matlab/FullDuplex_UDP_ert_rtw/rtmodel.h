619d8c1e78bd1860b8565848b834f19286109e2b
RorroRojas3 <rrojas@clemson.edu>
2018-12-02 23:34:08 -0500

    final

--- Tiva_VS_Demo_2018F/Tiva_UDP_Demo/Tiva_UDP_Demo/matlab/FullDuplex_UDP_ert_rtw/rtmodel.h
+++ Tiva_VS_Demo_2018F/Tiva_UDP_Demo/Tiva_UDP_Demo/matlab/FullDuplex_UDP_ert_rtw/rtmodel.h
@@ -8,9 +8,9 @@
  *
  * Code generated for Simulink model 'FullDuplex_UDP'.
  *
- * Model version                  : 1.145
+ * Model version                  : 1.148
  * Simulink Coder version         : 8.12 (R2017a) 16-Feb-2017
- * C/C++ source code generated on : Mon Nov 26 15:48:11 2018
+ * C/C++ source code generated on : Mon Nov 26 17:56:29 2018
  *
  * Target selection: ert.tlc
  * Embedded hardware selection: ARM Compatible->ARM Cortex
